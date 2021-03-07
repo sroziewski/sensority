d01a93687db40d56f07457f17b06c81582c2f7d2
Simon <sroziewski@opi.org.pl>
2021-03-07 22:59:43 +0100

    Starter

---
+++ src/test/java/sensoritymain/sensority/SensorityApplicationTests.java
@@ -0,0 +1,13 @@
+package sensoritymain.sensority;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
