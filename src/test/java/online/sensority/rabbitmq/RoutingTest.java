4052122b2d96dc1e0b6c49597aad0af052591c23
Simon <sroziewski@opi.org.pl>
2021-03-17 13:35:01 +0100

    mongodb test in embedded way

--- src/test/java/online/sensority/rabbitmq/RoutingTest.java
+++ src/test/java/online/sensority/rabbitmq/RoutingTest.java
@@ -1,44 +1,44 @@
-//package online.sensority.rabbitmq;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.amqp.rabbit.core.RabbitTemplate;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.testcontainers.containers.GenericContainer;
-//import org.testcontainers.containers.RabbitMQContainer;
-//import org.testcontainers.junit.jupiter.Container;
-//import org.testcontainers.junit.jupiter.Testcontainers;
-//import org.testcontainers.utility.DockerImageName;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@Testcontainers
-//@ContextConfiguration(classes = RabbitMqTestConfiguration.class)
-//public class RoutingTest {
-//
-//    private RabbitMQContainer underTest;
-//
-//    @Autowired
-//    private RabbitTemplate rabbitTemplate;
-//
-//    // container {
-//    @Container
-//    public RabbitMQContainer rabbitmq = new RabbitMQContainer(DockerImageName.parse("rabbitmq"))
-//            .withExposedPorts(5672);
-//    // }
-//
-//
-//    @BeforeEach
-//    public void setUp() {
-//        String address = rabbitmq.getHost();
-//        Integer port = rabbitmq.getFirstMappedPort();
-//
-//
-//    }
-//
-//    @Test
-//    public void testSimplePutAndGet() {
-//        int i=1;
-//    }
-//}
+package online.sensority.rabbitmq;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Testcontainers
+@ContextConfiguration(classes = RabbitMqTestConfiguration.class)
+public class RoutingTest {
+
+    private RabbitMQContainer underTest;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    // container {
+    @Container
+    public RabbitMQContainer rabbitmq = new RabbitMQContainer(DockerImageName.parse("rabbitmq"))
+            .withExposedPorts(5672);
+    // }
+
+
+    @BeforeEach
+    public void setUp() {
+        String address = rabbitmq.getHost();
+        Integer port = rabbitmq.getFirstMappedPort();
+
+
+    }
+
+    @Test
+    public void testSimplePutAndGet() {
+        int i=1;
+    }
+}
